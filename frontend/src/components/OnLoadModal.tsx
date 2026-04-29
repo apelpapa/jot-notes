4a7c3905f44fa681277ed83e6093f5420222a40e
Abel Papazian <apelpapa@gmail.com>
2026-04-29 15:09:04 -0400

    Update OnLoadModal.tsx

--- frontend/src/components/OnLoadModal.tsx
+++ frontend/src/components/OnLoadModal.tsx
@@ -11,12 +11,14 @@ export default function OnLoadModal() {
           <ModalContainer>
             <div className="bg-100 p-3 max-w-100">
               <p>
-                This App is currently in beta testing and every person is logged
-                in to the same user automatically and all notes are{" "}
-                <strong>public</strong>. Do not use this to store anything. It
-                is currently <strong>Wiped Every 30 to 60 Minutes</strong>. Have
-                fun, but <strong>PLEASE</strong> keep it appropriate. <br />
+                This app is currently in alpha testing. Everyone is
+                automatically logged in as the same user, and all notes are{" "}
+                <strong>public</strong>. Do not use this to store anything
+                private or important. Data is currently{" "}
+                <strong>wiped every 30 to 60 minutes</strong>. Have fun, but
+                please keep it appropriate.
               </p>
+
               <button
                 className="block btn btn-accent mx-auto mt-2"
                 onClick={() => setOnLoadModal(false)}
